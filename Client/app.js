2d8e8cf45e0f71a5e4090bdfe0e985de0615b6c0
Clint <crob2099@gmail.com>
2020-12-02 02:32:18 -0500

    Added background image, navbar-brand, header, put movie info inside of bootstrap cards and styled buttons

--- Client/app.js
+++ Client/app.js
@@ -20,7 +20,7 @@
             }
         });
 
-        e.preventDefault();
+         e.preventDefault();
     }
 
     $('#my-form').submit( processForm );
@@ -29,13 +29,17 @@
 $(function(){ 
     $.get("https://localhost:44325/api/movie").then(function(data){
         data.map(function(el){            
-            $("#movieList").append(`<div>
+            $("#movieList").append(`
+            <div class="col-lg-10 mb-4">
+            <div class="card bg-dark text-white text-center w-25 p-3">
+            <img src="Images/hotpopcornmovie.png" width="100" height="100">
             <div>Title:&nbsp;<span id="movieTitle${el.movieId}">${JSON.stringify(el.title)}</span></div>
             <div>Director:&nbsp;<span id="movieDirector${el.movieId}">${JSON.stringify(el.director)}</span></div>
             <div>Genre:&nbsp;<span id="movieGenre${el.movieId}">${JSON.stringify(el.genre)}</span></div>
-            <button type="button" onclick="putFunction(${el.movieId})">Edit</button>
-            <button type="button" onclick="deleteFunction(${el.movieId})">Delete</button>
-            </div><br>`)
+            <button type="button" class="btn btn-light" onclick="putFunction(${el.movieId})">Edit</button>
+            <button type="button" class="btn btn-danger" onclick="deleteFunction(${el.movieId})">Delete</button>
+            </div>
+            <div><br>`)
         })
     })
 
@@ -47,14 +51,18 @@ function putFunction(id){
         $.get(pathToMovie, function(data){
             console.log(data);
             document.getElementById("movieList").innerHTML = " ";
-            $("#movieList").append(`<div>
+            $("#movieList").append(`
+                <div class="col-lg-10 mb-5">
+                <div class="card bg-dark text-white text-center w-25 p-3">
+                <img src="Images/hotpopcornmovie.png" width="100" height="100">
                 <div>Title: <span id="movieTitle${data.movieId}">${JSON.stringify(data.title)}</span></div>
                 <div>Director: <span id="movieDirector${data.movieId}">${JSON.stringify(data.director)}</span></div>
                 <div>Genre: <span id="movieGenre${data.movieId}">${JSON.stringify(data.genre)}</span</div>
                 <br>
-                <button type="button" onclick="putRequestFunction(${data.movieId})">Save Changes</button>
-                <button type="button" onclick="reloadPage()">Refresh Movie List</button>
-            </div>`)
+                <button type="button" class="btn btn-success" onclick="putRequestFunction(${data.movieId})">Save Changes</button>
+                <button type="button" class="btn btn-warning" onclick="reloadPage()">Refresh Movie List</button>
+                </div>
+                </div>`)
         })
     
     })
@@ -105,14 +113,20 @@ function deleteFunction(id){
         $.get(pathToMovie, function(data){
             console.log(data);
             document.getElementById("movieList").innerHTML = " ";
-            $("#movieList").append(`<div>
+            $("#movieList").append(`
+                <div class="col-lg-10 mb-5">
+                <div class="card bg-dark text-white text-center w-25 p-3">
+                <img src="Images/hotpopcornmovie.png" width="100" height="100">
+                <div>
                 <div>Title: <span id="movieTitle${data.movieId}">${JSON.stringify(data.title)}</span></div>
                 <div>Director: <span id="movieDirector${data.movieId}">${JSON.stringify(data.director)}</span></div>
                 <div>Genre: <span id="movieGenre${data.movieId}">${JSON.stringify(data.genre)}</span</div>
                 <br>
-                <button type="button" onclick="deleteRequestFunction(${data.movieId})">Delete Movie</button>
-                <button type="button" onclick="reloadPage()">Refresh Movie List</button>
-            </div>`)
+                <button type="button" class="btn btn-danger" onclick="deleteRequestFunction(${data.movieId})">Delete Movie</button>
+                <button type="button" class="btn btn-warning" onclick="reloadPage()">Refresh Movie List</button>
+                </div>
+                </div>
+                </div>`)
         })
     
     })
